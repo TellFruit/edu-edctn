e787fd49658ae333a2f67b7f5a6846eabbd7c9c4
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-07-13 13:49:28 +0300

    Create User individual perk model

---
+++ backend/Portal.DAL/Entities/UserPerk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.DAL.Entities
+{
+    public class UserPerk
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public User User { get; set; }
+        public Perk Perk { get; set; }
+    }
+}
