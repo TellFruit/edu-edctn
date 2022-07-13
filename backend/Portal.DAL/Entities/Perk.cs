1f804363daf87ef2bd3435dce9c0ab1bf24b7d53
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-07-13 13:12:59 +0300

    Create Perk model

---
+++ backend/Portal.DAL/Entities/Perk.cs
@@ -0,0 +1,14 @@
+﻿using Portal.DAL.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.DAL.Entities
+{
+    public class Perk : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
