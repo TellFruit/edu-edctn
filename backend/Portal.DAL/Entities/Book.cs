a0e057b1f6fbd84a1b85517f40b4edb1a036f447
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-07-13 13:02:20 +0300

    Add authors list to Book model

--- backend/Portal.DAL/Entities/Book.cs
+++ backend/Portal.DAL/Entities/Book.cs
@@ -1,11 +1,18 @@
 ﻿using Portal.DAL.Entities.Abstract;
+using System.Collections;
 
 namespace Portal.DAL.Entities
 {
     public class Book : Material
     {
+        public Book()
+        {
+            Authors = new List<Author>();
+        }
+
         public int PageCount { get; set; }
         public string Format { get; set; }
         public DateTime Published { get; set; }
+        public ICollection Authors { get; private set; }
     }
 }
