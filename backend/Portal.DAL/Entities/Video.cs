e28b2a83a0c207b35db5974f28db4ad840123830
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-07-13 13:10:38 +0300

    Create Video model

---
+++ backend/Portal.DAL/Entities/Video.cs
@@ -0,0 +1,15 @@
+﻿using Portal.DAL.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.DAL.Entities
+{
+    public class Video : Material
+    {
+        public TimeSpan Duration { get; set; }
+        public int Quality { get; set; }
+    }
+}
