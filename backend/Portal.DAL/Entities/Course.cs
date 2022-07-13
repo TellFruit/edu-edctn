dd201893ced5a9192533d2e98d898337de49af05
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-07-13 13:45:52 +0300

    Create Course model

---
+++ backend/Portal.DAL/Entities/Course.cs
@@ -0,0 +1,22 @@
+﻿using Portal.DAL.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.DAL.Entities
+{
+    public class Course : BaseEntity
+    {
+        public Course()
+        {
+            Materials = new List<Material>();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public User Author { get; set; }
+        public ICollection<Material> Materials { get; set; }
+    }
+}
