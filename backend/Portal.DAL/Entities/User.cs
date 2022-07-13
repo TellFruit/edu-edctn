c25e511b21e4623ee05291d6a0542186b9216c6a
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-07-13 12:29:31 +0300

    Finish User model properties

--- backend/Portal.DAL/Entities/User.cs
+++ backend/Portal.DAL/Entities/User.cs
@@ -8,6 +8,10 @@ namespace Portal.DAL.Entities
 {
     public class User
     {
-
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
