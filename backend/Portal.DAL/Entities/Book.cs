7b3e1e90475e9362b47747b4cdf9b3110833af88
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-07-13 12:53:07 +0300

    Create Book model

---
+++ backend/Portal.DAL/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿using Portal.DAL.Entities.Abstract;
+
+namespace Portal.DAL.Entities
+{
+    public class Book : Material
+    {
+        public int PageCount { get; set; }
+        public string Format { get; set; }
+        public DateTime Published { get; set; }
+    }
+}
