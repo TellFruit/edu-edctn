c9cc0d9dcad524263aabd2eca062aa691dcb734a
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-03 10:31:16 +0300

    Create Solution folder structure and put current projects inside

--- backend/Portal.DAL/Entities/Article.cs
+++ backend/Portal.DAL/Entities/Article.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Portal.DAL.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Portal.DAL.Entities
 {
-    internal class Article
+    public class Article : Material
     {
+        public string Url { get; set; }
+        public DateTime Published { get; set; }
     }
 }
