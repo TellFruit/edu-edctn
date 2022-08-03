1d1f8f726b68704b5480327d91b5d73a59084358
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-03 21:25:57 +0300

    Start JsonSerialzationService implementation of ISerializationService + create file for global usings

---
+++ backend/Portal.Persistence-Json/Services/JsonSerializationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence_Json.Services
+{
+    internal class JsonSerializationService : ISerializationService
+    {
+        public T Deserialize<T>(string serialized)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize<T>(T unserialized)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
