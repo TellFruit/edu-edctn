c1675497d86a7ffbf2228374dfe405c68fe9bbb3
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-03 21:19:48 +0300

    Start FileIoService implementation of IFileIoService

---
+++ backend/Portal.Persistence-Json/Services/FileIoService.cs
@@ -0,0 +1,22 @@
+﻿using Portal.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence_Json.Services
+{
+    internal class FileIoService : IFileIoService
+    {
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
