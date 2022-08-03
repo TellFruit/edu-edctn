e21f06235757fb13bfc3b3739a43766423c13749
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-03 21:07:02 +0300

    Create basic ISerialiazationHelper for (de)serialization actions

---
+++ backend/Portal.DAL/Interfaces/ISerializationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Domain.Interfaces
+{
+    internal interface ISerializationService
+    {
+        string Serialize<T>(T obj);
+
+        T Deserialize<T>(string json);
+    }
+}
