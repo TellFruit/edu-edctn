092a2f89336fa33ca26dbb61185a67a5d2619189
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-04 13:12:49 +0300

    Start JsonGenericRepository implementation of IGenericRepository

--- backend/Portal.Persistence-Json/ProjectUsings.cs
+++ backend/Portal.Persistence-Json/ProjectUsings.cs
@@ -1,3 +1,7 @@
 ﻿global using Portal.Domain.Interfaces;
+global using Portal.Domain.Entities;
+global using Portal.Domain.Entities.Abstract;
+
+global using Portal.Application.Interfaces.Outer;
 
 global using Newtonsoft.Json;
---
+++ backend/Portal.Persistence-Json/Repositories/JsonGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using Portal.Application.Interfaces.Outer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence_Json.Repositories
+{
+    internal class JsonGenericRepository : IGenericRepository
+    {
+    }
+}
