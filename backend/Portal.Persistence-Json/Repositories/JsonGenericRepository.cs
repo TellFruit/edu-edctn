1d319a7676786989b76d30760c1847c79f67efbf
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-04 15:20:29 +0300

    JsonGenericRepository add services DI and InitEntities method to use them

--- backend/Portal.Persistence-Json/Repositories/JsonGenericRepository.cs
+++ backend/Portal.Persistence-Json/Repositories/JsonGenericRepository.cs
@@ -1,5 +1,4 @@
-﻿using Portal.Application.Interfaces.Outer;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Portal.Persistence_Json.Repositories
 {
-    internal class JsonGenericRepository : IGenericRepository
+    internal class JsonGenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
+        private readonly IFileIoService _file;
+        private readonly ISerializationService _json;
+
+        private ICollection<T> _entities;
+
+        public JsonGenericRepository(IFileIoService file, ISerializationService json)
+        {
+            _file = file;
+            _json = json;
+
+            InitEntities();
+        }
+
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<T> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitEntities()
+        {
+            try
+            {
+                _entities = _json.Deserialize<List<T>>(_file.Read());
+            }
+            catch (ArgumentException)
+            {
+                _entities = new List<T>();
+            }
+        }
     }
 }
