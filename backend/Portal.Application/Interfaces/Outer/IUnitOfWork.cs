882775b9bb6c0a199bc0058b1ddfb832c7360d0c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-04 12:43:42 +0300

    Create IBaseUnitOfWork interface

---
+++ backend/Portal.Application/Interfaces/Outer/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Portal.Domain.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Interfaces.Outer
+{
+    public interface IBaseUnitOfWork
+    {
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity;
+        void Save();
+    }
+}
