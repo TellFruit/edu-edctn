766705c1d29f1a7bfd49f76fe6ca1fcde41db620
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 14:10:06 +0300

    Create PerkDTO

---
+++ backend/Portal.Application/DTO/PerkDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.DTO
+{
+    internal class PerkDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
