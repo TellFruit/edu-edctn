67d5d890009b7f7a38c84a5697391a9dc64b3fcb
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 12:39:49 +0300

    Start VideoService implementation of IModelService

---
+++ backend/Portal.Application/Services/VideoService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Services
+{
+    internal class VideoService : BaseModelService<Video> IModelService<VideoDTO>
+    {
+        public VideoService(IGenericRepository<Video> repository, IMapper mapper)
+            : base(repository, mapper) {}
+
+        public Task<VideoDTO> Create(VideoDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<VideoDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VideoDTO> Update(VideoDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
