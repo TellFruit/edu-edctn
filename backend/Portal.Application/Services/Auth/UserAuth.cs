597106d929bd45c9bd914844d223465e35ff1cbc
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 01:48:14 +0300

    Start UserAuth implementation of IUserAuth

---
+++ backend/Portal.Application/Services/Auth/UserAuth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Services.Auth
+{
+    internal class UserAuth : IUserAuth
+    {
+        private readonly IGenericRepository<User> _repository;
+        private readonly UserDTO _user;
+
+        private bool _authenticated;
+
+        public UserAuth(IGenericRepository<User> repository)
+        {
+            _repository = repository;
+            _user = new UserDTO();  
+            _authenticated = false;
+        }
+
+        public bool IsAuthenticated => _authenticated;
+
+        public Task<bool> Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Register(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
