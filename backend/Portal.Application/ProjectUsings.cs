cfba21250539251ecd154becb56f31c4f4aff105
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 03:03:09 +0300

    Start ArticleService implementation of IModelService

--- backend/Portal.Application/ProjectUsings.cs
+++ backend/Portal.Application/ProjectUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using Portal.Application.Interfaces.Outer;
+global using Portal.Application.Interfaces.Inner;
 global using Portal.Domain.Entities.Abstract;
+global using Portal.Domain.Entities;
---
+++ backend/Portal.Application/Services/ArticleService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Services
+{
+    internal class ArticleService : IModelService<Article>
+    {
+        public Task<Article> Create(Article entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(Article entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Article>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> Update(Article entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
