111ae69b5378624e528e29e53bc4e39f97d97a74
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 21:10:23 +0300

    JsonSerializationService rename to NewtonJsonService + add settings field + update to use it

---
+++ backend/Portal.Persistence-Json/Services/NewtonJsonService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence_Json.Services
+{
+    internal class NewtonJsonService : ISerializationService
+    {
+        private readonly JsonSerializerSettings _settings;
+
+        public NewtonJsonService()
+        {
+            _settings = new JsonSerializerSettings {
+                TypeNameHandling = TypeNameHandling.Auto,
+                Formatting = Formatting.Indented };
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            return (T)JsonConvert.DeserializeObject(json, typeof(T), _settings);
+        }
+
+        public string Serialize<T>(T obj)
+        {
+            string result = JsonConvert.SerializeObject(obj, _settings);
+
+            return result.Length > 1 ? result : throw new ArgumentException(nameof(obj));
+        }
+    }
+}
