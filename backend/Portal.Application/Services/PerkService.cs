bcc69998c33ba2f5a1b0c61a3fa572b0c06f39e2
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 21:58:25 +0300

    PerService finish Delete method

--- backend/Portal.Application/Services/PerkService.cs
+++ backend/Portal.Application/Services/PerkService.cs
@@ -23,9 +23,17 @@ namespace Portal.Application.Services
             return _mapper.Map<PerkDTO>(perk);
         }
 
-        public Task<int> Delete(int id)
+        public async Task<int> Delete(int id)
         {
-            throw new NotImplementedException();
+            var perks = await _repository.Read();
+            var toDelete = perks.FirstOrDefault(a => a.Id.Equals(id));
+
+            if (toDelete is null)
+            {
+                throw new EntityNotFoundException(nameof(Perk));
+            }
+
+            return await _repository.Delete(toDelete);
         }
 
         public Task<ICollection<PerkDTO>> GetAll()
