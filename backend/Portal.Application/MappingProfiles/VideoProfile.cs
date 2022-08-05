3d22c384963951c68bd1f60ebcabf07b13f3c64a
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 12:38:01 +0300

    Create Video mapping profile

---
+++ backend/Portal.Application/MappingProfiles/VideoProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.MappingProfiles
+{
+    internal class VideoProfile : Profile
+    {
+        public VideoProfile()
+        {
+            CreateMap<Video, VideoDTO>();
+
+            CreateMap<VideoDTO, Video>();
+        }
+    }
+}
