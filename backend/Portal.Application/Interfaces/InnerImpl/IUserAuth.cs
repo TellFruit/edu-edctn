9e87d51ff4d88115e9fbdac9d8dab9a71c7b7782
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 01:29:19 +0300

    IUseAuth update method Register and delete set accessor

--- backend/Portal.Application/Interfaces/InnerImpl/IUserAuth.cs
+++ backend/Portal.Application/Interfaces/InnerImpl/IUserAuth.cs
@@ -8,8 +8,8 @@ namespace Portal.Application.Interfaces.InnerImpl
 {
     public interface IUserAuth
     {
-        public bool IsAuthenticated { get; set; }
+        public bool IsAuthenticated { get; }
         Task<bool> Login(string email, string password);
-        Task Register(string email, string password);
+        Task<bool> Register(string email, string password);
     }
 }
