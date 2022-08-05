56b8f7114ba0cfc3758ccfe853930d995f6093a2
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 04:18:36 +0300

    Create NotFoundException

---
+++ backend/Portal.DAL/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Domain.Exceptions
+{
+    internal class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string name)
+            : base($"Entity of type ({name}) not found!")
+        {
+        }
+    }
+}
