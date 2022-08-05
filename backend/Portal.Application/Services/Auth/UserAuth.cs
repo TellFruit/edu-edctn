caf1aa3674de9867443cb7b581e5bb7d5301b13e
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 01:54:04 +0300

    UserAuth finish Login method

--- backend/Portal.Application/Services/Auth/UserAuth.cs
+++ backend/Portal.Application/Services/Auth/UserAuth.cs
@@ -9,22 +9,36 @@ namespace Portal.Application.Services.Auth
     internal class UserAuth : IUserAuth
     {
         private readonly IGenericRepository<User> _repository;
-        private readonly UserDTO _user;
+        private readonly IMapper _mapper;
+        private UserDTO _user;
 
         private bool _authenticated;
 
-        public UserAuth(IGenericRepository<User> repository)
+        public UserAuth(IGenericRepository<User> repository, IMapper mapper)
         {
             _repository = repository;
-            _user = new UserDTO();  
+            _mapper = mapper;
+            _user = new UserDTO();
             _authenticated = false;
         }
 
         public bool IsAuthenticated => _authenticated;
 
-        public Task<bool> Login(string email, string password)
+        public async Task<bool> Login(string email, string password)
         {
-            throw new NotImplementedException();
+            var users = await _repository.Read();
+            var found = users.FirstOrDefault(u => !u.Email.Equals(email)
+               && !u.Password.Equals(password));
+
+            if (found is null)
+            {
+                return false;
+            }
+
+            _authenticated = true;
+            _user = _mapper.Map<UserDTO>(found);
+
+            return _authenticated;
         }
 
         public Task<bool> Register(string email, string password)
