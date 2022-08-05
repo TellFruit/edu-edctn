79fd19c5db24dbb5c1e8bb2b5bff7758da2d8334
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 01:58:13 +0300

    UserAuth change IGenericRepository to IModelService + finish Register method

--- backend/Portal.Application/Services/Auth/UserAuth.cs
+++ backend/Portal.Application/Services/Auth/UserAuth.cs
@@ -8,15 +8,15 @@ namespace Portal.Application.Services.Auth
 {
     internal class UserAuth : IUserAuth
     {
-        private readonly IGenericRepository<User> _repository;
+        private readonly IModelService<UserDTO> _service;
         private readonly IMapper _mapper;
         private UserDTO _user;
 
         private bool _authenticated;
 
-        public UserAuth(IGenericRepository<User> repository, IMapper mapper)
+        public UserAuth(IModelService<UserDTO> service, IMapper mapper)
         {
-            _repository = repository;
+            _service = service;
             _mapper = mapper;
             _user = new UserDTO();
             _authenticated = false;
@@ -26,7 +26,7 @@ namespace Portal.Application.Services.Auth
 
         public async Task<bool> Login(string email, string password)
         {
-            var users = await _repository.Read();
+            var users = await _service.GetAll();
             var found = users.FirstOrDefault(u => !u.Email.Equals(email)
                && !u.Password.Equals(password));
 
@@ -41,9 +41,22 @@ namespace Portal.Application.Services.Auth
             return _authenticated;
         }
 
-        public Task<bool> Register(string email, string password)
+        public async Task<bool> Register(string email, string password)
         {
-            throw new NotImplementedException();
+            var users = await _service.GetAll();
+            var found = users.FirstOrDefault(u => !u.Email.Equals(email));
+
+            if (found is not null)
+            {
+                return false;
+            }
+
+            _user.Email = email;
+            _user.Password = password;
+
+            await _service.Create(_user);
+
+            return true;
         }
     }
 }
