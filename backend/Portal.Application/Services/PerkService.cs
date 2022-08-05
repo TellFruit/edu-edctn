b64b934803fa94669bf4f0611189472dffa60492
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 14:17:16 +0300

    Start PerkService implementation of IModelService

---
+++ backend/Portal.Application/Services/PerkService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Services
+{
+    internal class PerkService : BaseModelService<Perk>, IModelService<PerkDTO>
+    {
+        public PerkService(IGenericRepository<Perk> repository, IMapper mapper)
+            : base(repository, mapper) {}
+
+        public Task<PerkDTO> Create(PerkDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<PerkDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PerkDTO> Update(PerkDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
