7b436cbd9443a5a1b930ef84fed68599fa7e28a7
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 22:15:16 +0300

    Start UserService implementation of IModelService

---
+++ backend/Portal.Application/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Services
+{
+    internal class UserService : BaseModelService<User>, IModelService<UserDTO>
+    {
+        public UserService(IGenericRepository<User> repository, IMapper mapper)
+            : base(repository, mapper) {}
+
+        public Task<UserDTO> Create(UserDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<UserDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO> Update(UserDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
