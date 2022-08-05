29e53fc0957a567d0b63b831359858e75c849132
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-05 14:12:22 +0300

    Create and finish PerkProfile

--- backend/Portal.Application/MappingProfiles/PerkProfile.cs
+++ backend/Portal.Application/MappingProfiles/PerkProfile.cs
@@ -8,5 +8,11 @@ namespace Portal.Application.MappingProfiles
 {
     internal class PerkProfile : Profile
     {
+        public PerkProfile()
+        {
+            CreateMap<Perk, PerkDTO>();
+
+            CreateMap<PerkDTO, Perk>();
+        }
     }
 }
