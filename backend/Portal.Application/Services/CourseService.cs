495c37c062908d40d1ff5ace30cf6540653a1436
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 14:14:27 +0300

    Start CourseService implementation of ICourseService + finish Create method

--- backend/Portal.Application/Services/CourseService.cs
+++ backend/Portal.Application/Services/CourseService.cs
@@ -1,7 +1,35 @@
 ﻿namespace Portal.Application.Services
 {
-    public class CourseService
+    internal class CourseService : BaseModelService<Course>, ICourseService
     {
+        public CourseService(IGenericRepository<Course> repository, IMapper mapper)
+            : base(repository, mapper) {}
 
+        public async Task<CourseDTO> Create(CourseDTO entity)
+        {
+            var course = _mapper.Map<Course>(entity);
+
+            course.CreatedAt = DateTime.Now;
+
+            await _repository.Create(course);
+            await _repository.SaveChanges();
+
+            return _mapper.Map<CourseDTO>(course);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<CourseDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CourseDTO> Update(CourseDTO entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
