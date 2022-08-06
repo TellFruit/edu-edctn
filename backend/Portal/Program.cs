99fca82ba6d83bd76662fb1485c1fa9a99c47c3e
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 14:49:48 +0300

    Start Console app + make Program be root for dependency injection

--- backend/Portal/Program.cs
+++ backend/Portal/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Portal.UI_Console
+{
+    [ExcludeFromCodeCoverage]
+    static class Program
+    {
+        private static IServiceProvider _root;
+        public static IServiceProvider Root => _root;
+
+        private static IServiceProvider CompositionRoot()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<ConsoleApp>();
+
+            Portal.Application
+                .DependencyInjection.RegisterApplication(services);
+            Portal.Persistence_Json
+                .DependencyInjection.RegisterPersistenceJson(services);
+
+            return services.BuildServiceProvider();
+        }
+
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            _root = CompositionRoot();
+
+            Root.GetService<ConsoleApp>().Run();
+        }
+    }
+}
