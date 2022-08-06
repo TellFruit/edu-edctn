34c826ac6deb06f0b3d8745d5b06528043afcd41
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 21:54:41 +0300

    CommanManager add ctor and + interface fields and properties

--- backend/Portal/CommandManager.cs
+++ backend/Portal/CommandManager.cs
@@ -8,6 +8,19 @@ namespace Portal.UI_Console
 {
     internal class CommandManager : ICommandManager
     {
+        private readonly IConfigService _config;
+
+        private IParseInput _commandParser;
+        private IParseInput? _parameterParser;
+
+        public IConsoleCommand? LastCommand { get; set; }
+
+        public CommandManager(IParseInput commandParser, IConfigService config)
+        {
+            _commandParser = commandParser;
+            _config = config;
+        }
+
         public IConsoleCommand? GetCommand(string commandName)
         {
             throw new NotImplementedException();
--- backend/Portal/ProjectUsings.cs
+++ backend/Portal/ProjectUsings.cs
@@ -1,7 +1,8 @@
 ﻿global using System.Diagnostics.CodeAnalysis;
+global using System.Text.RegularExpressions;
 global using System.Text;
 
+global using Portal.Application.Interfaces.InnerImpl;
 global using Portal.UI_Console.Interfaces;
-global using System.Text.RegularExpressions;
 
 global using Microsoft.Extensions.DependencyInjection;
