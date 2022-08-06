6b5e7bdf709ea71903f2691bf84bbb96d7c543a0
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 18:41:01 +0300

    Create IConfigService interface

---
+++ backend/Portal.Application/Interfaces/InnerImpl/IConfigService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Interfaces.InnerImpl
+{
+    public interface IConfigureService
+    {
+        string GetSetting(string key);
+    }
+}
