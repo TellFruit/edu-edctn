e7fb42682b1bf79af0c6936109144c14eb4ccd10
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 11:45:02 +0300

    Application proj add DependencyInjection sattic class with register method

---
+++ backend/Portal.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿namespace Portal.Application
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterApplication(IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddProfile<ArticleProfile>();
+                cfg.AddProfile<BookProfile>();
+                cfg.AddProfile<VideoProfile>();
+                cfg.AddProfile<PerkProfile>();
+                cfg.AddProfile<UserProfile>();
+            },
+            Assembly.GetExecutingAssembly());
+
+            services.AddScoped<ArticleService>();
+            services.AddScoped<BookService>();
+            services.AddScoped<VideoService>();
+            services.AddScoped<PerkService>();
+            services.AddScoped<UserService>();
+
+            services.AddScoped<IUserAuth, UserAuth>();
+        }
+    }
+}
--- backend/Portal.Application/ProjectUsings.cs
+++ backend/Portal.Application/ProjectUsings.cs
@@ -1,10 +1,17 @@
-﻿global using Portal.Application.Interfaces.OuterImpl;
+﻿global using System.Reflection;
+
+global using Portal.Application.Interfaces.OuterImpl;
 global using Portal.Application.Interfaces.InnerImpl;
+global using Portal.Application.MappingProfiles;
 global using Portal.Application.Services.Abstract;
+global using Portal.Application.Services.Auth;
+global using Portal.Application.Services;
 global using Portal.Application.DTO;
 
 global using Portal.Domain.Entities;
 global using Portal.Domain.Entities.Abstract;
 global using Portal.Domain.Exceptions;
+
+global using Microsoft.Extensions.DependencyInjection;
 
 global using AutoMapper;
