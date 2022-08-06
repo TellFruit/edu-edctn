475ebdff80c1ec484370d3a2d7cb762e6504f224
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 21:52:00 +0300

    Start CommandManager implementation of ICommandManager

---
+++ backend/Portal/CommandManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console
+{
+    internal class CommandManager : ICommandManager
+    {
+        public IConsoleCommand? GetCommand(string commandName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitCommandFlow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCommandParser(IParseInput parser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
