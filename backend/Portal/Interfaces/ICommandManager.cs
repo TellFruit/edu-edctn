1cbe8358913e551670e334df04356e5ab4060c39
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-06 18:38:14 +0300

    Create ICommandManager interface

---
+++ backend/Portal/Interfaces/ICommandManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.Interfaces
+{
+    internal interface ICommandManager
+    {
+        void InitCommandFlow();
+
+        void SetCommandParser(IParseInput parser);
+
+        IConsoleCommand? GetCommand(string commandName);
+    }
+}
