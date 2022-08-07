79920502ed5ed2abd3bcbe428e8b3d14375db5f7
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-07 17:31:43 +0300

    AddArticleCommand almost finish Run method without doing adding itself

--- backend/Portal/ConsoleCommands/Modify/Course/AddArticleCommand.cs
+++ backend/Portal/ConsoleCommands/Modify/Course/AddArticleCommand.cs
@@ -8,9 +8,48 @@ namespace Portal.UI_Console.ConsoleCommands.Modify.Course
 {
     internal class AddArticleCommand : IConsoleCommand
     {
-        public Task<bool> Run(params string[] parameters)
+        private readonly IArticleService _article;
+
+        private readonly CourseDTO _courseDTO;
+
+        public AddArticleCommand(IArticleService articleService, CourseDTO courseDTO)
         {
-            throw new NotImplementedException();
+            _article = articleService;
+            _courseDTO = courseDTO;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            var configService = Program.Root.GetService<IConfigService>();
+
+            var articles = await _article.GetAll();
+
+            var viewCommand = new PaginateCommand<ArticleDTO>(configService, articles);
+
+            Console.WriteLine("Take a look and write \'return\' to proceed and choose");
+            await viewCommand.Run();
+
+            int wantedId = 0;
+            string input = "";
+            do
+            {
+                Console.WriteLine("Write id of wanted article or \'return\' to stop: ");
+
+                input = Console.ReadLine();
+
+                if (input.Equals("return"))
+                {
+                    break;
+                }
+
+                try { wantedId = int.Parse(input); }
+                catch { if (!input.Equals("")) { input = ""; continue; } }
+
+                break;
+            }
+            while (true);
+
+            return true;
         }
     }
 }
