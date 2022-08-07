e207b912c32ec9ec532d5407ad8192d1ee4f97d1
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-07 16:33:25 +0300

    Start and finish PaginateCommand implementation of IConsoleCommand + create CommandExtensions with Paginate mathod

---
+++ backend/Portal/ConsoleCommands/CommandExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands
+{
+    internal static class CommandExtension
+    {
+        public static ICollection<T> GetPaginatedPage<T>(this ICollection<T> collection, int page, int pageSize)
+        {
+            return collection.Skip((page - 1) * pageSize)
+                                          .Take(pageSize)
+                                          .ToList();
+        }
+    }
+}
---
+++ backend/Portal/ConsoleCommands/Other/PaginateCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Other
+{
+    internal class PaginateCommand<T> : IConsoleCommand
+    {
+        private ICollection<T> _collection;
+
+        public PaginateCommand(ICollection<T> collection)
+        {
+            _collection = collection;
+        }
+
+        public async Task Output(params string[] parameters)
+        {
+            string input = "";
+
+            int page = 1;
+            int pageSize = 3;
+
+            do
+            {
+                if (input.Equals("return"))
+                {
+                    break;
+                }
+
+                try { page = int.Parse(input); }
+                catch { if (!input.Equals("")) { input = ""; continue; } }
+
+                Console.WriteLine($"Page {page}:");
+
+                ICollection<T> paginated = _collection.GetPaginatedPage(page, pageSize);
+
+                foreach (T item in paginated)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("Next page or \'return\': ");
+                input = Console.ReadLine();
+            }
+            while (true);
+        }
+    }
+}
