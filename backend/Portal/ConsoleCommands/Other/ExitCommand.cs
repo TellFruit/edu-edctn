89300e12847e1c5a2384777e73591134c8caea8f
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-07 16:39:42 +0300

    Start and finish PaginateCommand implementation of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Other/ExitCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Other
+{
+    internal class ExitCommand : IConsoleCommand
+    {
+        public async Task Output(params string[] parameters)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
