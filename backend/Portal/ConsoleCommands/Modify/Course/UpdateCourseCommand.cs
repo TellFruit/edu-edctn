2d9576c48a0fcd67df129e14361ffa7ca77725a9
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 16:27:04 +0300

    Finish UpdateCourseCommand

---
+++ backend/Portal/ConsoleCommands/Modify/Course/UpdateCourseCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Modify.Course
+{
+    internal class UpdateCourseCommand : IConsoleCommand
+    {
+        private readonly ICourseService _course;
+ 
+        public UpdateCourseCommand(ICourseService course)
+        {
+            _course = course;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            var userAuth = Program.Root.GetService<IUserAuth>();
+
+            var auth = new AuthorizeCommand(userAuth);
+            if (await auth.Run() is false)
+            {
+                Console.WriteLine("Operation suspended!");
+                return true;
+            }
+
+            var courseDTO = await _course.GetById(int.Parse(parameters[0]));
+
+            if (courseDTO is null)
+            {
+                return true;
+            }
+
+            var basicParser = Program.Root.GetService<IParseInput>();
+            var chooseCommand = new ChooseCourseCommand(courseDTO);
+
+            var commandManager = new CommandManager(basicParser, chooseCommand);
+
+            Console.WriteLine("Course update flow started. Write commands to update it.");
+            await commandManager.InitCommandFlow();
+
+            if (courseDTO.Materials.Count > 0)
+            {
+                await _course.Create(courseDTO);
+            }
+            else
+            {
+                Console.WriteLine("Operation failed. No materials are present!");
+            }
+
+            return true;
+        }
+    }
+}
