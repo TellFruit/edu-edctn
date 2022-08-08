a1af33d6cdecdafd60e33609559cb42ed5e4113c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 13:53:52 +0300

    Finish GetCoursesCommand impl of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Get/GetCoursesCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Get
+{
+    internal class GetCoursesCommand : IConsoleCommand
+    {
+        private readonly ICourseService _courseService;
+
+        public GetCoursesCommand(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            var allBooks = await _courseService.GetAll();
+
+            var configService = Program.Root.GetService<IConfigService>();
+
+            IConsoleCommand paginate = new PaginateCommand<CourseDTO>(configService, allBooks);
+
+            await paginate.Run();
+
+            return true;
+        }
+    }
+}
