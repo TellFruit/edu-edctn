e27e6bbce16c0a67dac2e1473d66f212987a5e93
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 15:51:22 +0300

    AutorizeCommand finish Run method

--- backend/Portal/ConsoleCommands/Auth/AuthorizeCommand.cs
+++ backend/Portal/ConsoleCommands/Auth/AuthorizeCommand.cs
@@ -22,13 +22,37 @@ namespace Portal.UI_Console.ConsoleCommands.Auth
                 return true;
             }
 
+            string input = "";
             do
             {
+                Console.WriteLine("Write 'login' or 'register'. To suspend - write 'return'");
+                input = Console.ReadLine();
 
+                if (input.Equals("return"))
+                {
+                    break;
+                }
+
+                if (input.Equals("login"))
+                {
+                    var login = new LoginCommand(_userAuth);
+                    await login.Run();
+                }
+
+                if (input.Equals("register"))
+                {
+                    var register = new RegisterCommand(_userAuth);
+                    await register.Run();
+                }
+
+                if (_userAuth.IsAuthenticated)
+                {
+                    break;
+                }
             }
             while (true);
 
-            return true;
+            return _userAuth.IsAuthenticated;
         }
     }
 }
