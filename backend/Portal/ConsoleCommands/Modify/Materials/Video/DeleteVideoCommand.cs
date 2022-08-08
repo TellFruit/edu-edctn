5975506b408eeb3cdc29337bef842220745deee5
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 15:12:05 +0300

    Finish DeleteVideoCommand impl of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Modify/Materials/Video/DeleteVideoCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Modify.Materials.Video
+{
+    internal class DeleteVideoCommand : IConsoleCommand
+    {
+        private readonly IVideoService _videoService;
+
+        public DeleteVideoCommand(IVideoService videoService)
+        {
+            _videoService = videoService;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            await _videoService.Delete(int.Parse(parameters[0]));
+            Console.WriteLine("Success! Video deleted");
+
+            return true;
+        }
+    }
+}
