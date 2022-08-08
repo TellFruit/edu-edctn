330ebbad84cf8cccdee4f89e262aa7663d4d8b9c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 16:20:30 +0300

    Finish DeleteCourseCommand impl of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Modify/Course/DeleteCourseCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Modify.Course
+{
+    internal class DeleteCourseCommand : IConsoleCommand
+    {
+        private readonly ICourseService _courseService;
+
+        public DeleteCourseCommand(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            var userAuth = Program.Root.GetService<IUserAuth>();
+
+            var auth = new AuthorizeCommand(userAuth);
+            if (await auth.Run() is false)
+            {
+                Console.WriteLine("Operation suspended!");
+                return true;
+            }
+
+            await _courseService.Delete(int.Parse(parameters[0]));
+            Console.WriteLine("Success! Course deleted");
+
+            return true;
+        }
+    }
+}
