660eae1c9e6a7e654593ead6158531c8c964dc02
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 14:48:33 +0300

    Finish DeleteArticleCommand impl of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Modify/Materials/Article/DeleteArticleCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Modify.Materials.Article
+{
+    internal class DeleteArticleCommand : IConsoleCommand
+    {
+        private readonly IArticleService _articleService;
+
+        public DeleteArticleCommand(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            await _articleService.Delete(int.Parse(parameters[0]));
+            Console.WriteLine("Success! Article deleted");
+
+            return true;
+        }
+    }
+}
