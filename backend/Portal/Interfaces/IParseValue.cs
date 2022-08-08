24ce83e4d7e3c327677574a5eacf92735323830c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 12:59:23 +0300

    Create IParseValue interface + finish its ParseDateTime implementation

---
+++ backend/Portal/Interfaces/IParseValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.Interfaces
+{
+    internal interface IParseValue<T> where T : struct
+    {
+        public T Parse(string input);
+
+    }
+}
---
+++ backend/Portal/Parsers/Values/ParseDateTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.Parsers.Values
+{
+    internal class ParseDateTime : IParseValue<DateTime>
+    {
+        private static string[] validFormats = new[] { "MM/dd/yyyy", "MM/dd/yyyy HH:mm:ss",
+                                                            "dd.MM.yyyy", "dd.MM.yyyy HH:mm:ss",
+                                                                "yyyy"};
+
+        public DateTime Parse(string input)
+        {
+            DateTime dateTime;
+
+            CultureInfo provider = CultureInfo.InvariantCulture;
+
+            DateTime.TryParseExact(input, validFormats, provider, DateTimeStyles.None, out dateTime);
+
+            if (!input.Contains('.') && !input.Contains('/'))
+            {
+                dateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day,
+                    1, 1, 1);
+            }
+
+            return dateTime;
+        }
+    }
+}
