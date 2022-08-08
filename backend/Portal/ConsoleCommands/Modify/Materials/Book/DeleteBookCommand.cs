25811c5b58c1680559f3b5daaaaa9050476a511a
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 15:08:30 +0300

    Finish DeletBookCommand impl of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Modify/Materials/Book/DeleteBookCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Modify.Materials.Book
+{
+    internal class DeleteBookCommand : IConsoleCommand
+    {
+        private readonly IBookService _bookService;
+
+        public DeleteBookCommand(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            await _bookService.Delete(int.Parse(parameters[0]));
+            Console.WriteLine("Success! Book deleted");
+
+            return true;
+        }
+    }
+}
