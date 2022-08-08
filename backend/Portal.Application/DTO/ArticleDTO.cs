c8d0910bf34835cc46fe72eb40d7f94fce8bd92b
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 13:12:19 +0300

    ArticleDTO override ToString

--- backend/Portal.Application/DTO/ArticleDTO.cs
+++ backend/Portal.Application/DTO/ArticleDTO.cs
@@ -1,9 +1,31 @@
-﻿namespace Portal.Application.DTO
+﻿using System.Globalization;
+
+namespace Portal.Application.DTO
 {
     public class ArticleDTO : MaterialDTO
     {
         public string Url { get; set; }
         public DateTime Published { get; set; }
         public ICollection<PerkDTO> Perks { get; set; }
+        public override string ToString()
+        {
+            string date;
+
+            if (Published.Hour == 1 && Published.Minute == 1 && Published.Second == 1)
+            {
+                date = Published.ToString("yyyy");
+            }
+            else if (CultureInfo.CurrentCulture.Name.ToLower().Contains("ua") ||
+                CultureInfo.CurrentCulture.Name.ToLower().Contains("ru"))
+            {
+                date = Published.ToString("dd.MM.yyyy");
+            }
+            else
+            {
+                date = Published.ToString("MM/dd/yyyy");
+            }
+
+            return $"{Title} - {date}, {Url}";
+        }
     }
 }
