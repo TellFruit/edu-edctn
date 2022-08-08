32c7fc5fb2f43abb8f61bcb697d8345a2e12298c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 13:03:18 +0300

    BookDTO override ToString

--- backend/Portal.Application/DTO/BookDTO.cs
+++ backend/Portal.Application/DTO/BookDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace Portal.Application.DTO
+﻿using System.Globalization;
+
+namespace Portal.Application.DTO
 {
     public class BookDTO : MaterialDTO
     {
@@ -7,5 +9,28 @@
         public DateTime Published { get; set; }
         public ICollection<string> Authors { get; set; }
         public ICollection<Perk> Perks { get; set; }
+
+        public override string ToString()
+        {
+            // template: Е. Таненбаум - Сучасні операційні системи, 1365 (2013).pdf
+
+            string date;
+
+            if (Published.Hour == 1 && Published.Minute == 1 && Published.Second == 1)
+            {
+                date = Published.ToString("yyyy");
+            }
+            else if (CultureInfo.CurrentCulture.Name.ToLower().Contains("ua") ||
+                CultureInfo.CurrentCulture.Name.ToLower().Contains("ru"))
+            {
+                date = Published.ToString("dd.MM.yyyy");
+            }
+            else
+            {
+                date = Published.ToString("MM/dd/yyyy");
+            }
+
+            return $"{string.Join(", ", Authors)} - {Title}, {PageCount} ({date}){Format}";
+        }
     }
 }
