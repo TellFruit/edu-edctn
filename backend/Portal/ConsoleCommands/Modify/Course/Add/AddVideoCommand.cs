b4560147dd850b3ca975cdfdbd531f82d8da7a1b
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 17:09:03 +0300

    Finish AddVideoCommand

---
+++ backend/Portal/ConsoleCommands/Modify/Course/Add/AddVideoCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Modify.Course.Add
+{
+    internal class AddVideoCommand : IConsoleCommand
+    {
+        private readonly IVideoService _video;
+
+        private readonly CourseDTO _courseDTO;
+
+        public AddVideoCommand(IVideoService videoService, CourseDTO courseDTO)
+        {
+            _video = videoService;
+            _courseDTO = courseDTO;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            var configService = Program.Root.GetService<IConfigService>();
+
+            var videos = await _video.GetAll();
+
+            var viewCommand = new PaginateCommand<VideoDTO>(configService, videos);
+
+            Console.WriteLine("Take a look and write \'return\' to proceed and choose");
+            await viewCommand.Run();
+
+            ICollection<int> marked = new List<int>();
+            int wantedId = 0;
+            string input = "";
+            do
+            {
+                Console.WriteLine("Write id of wanted video or \'return\' to stop: ");
+
+                input = Console.ReadLine();
+
+                if (input.Equals("return"))
+                {
+                    return true;
+                }
+
+                try { wantedId = int.Parse(input); }
+                catch { if (!input.Equals("")) { input = ""; continue; } }
+
+                if (marked.Contains(wantedId))
+                {
+                    Console.WriteLine("Already included!");
+                    continue;
+                }
+
+                var wantedArticle = await _video.GetById(wantedId);
+
+                _courseDTO.Materials.Add(wantedArticle);
+
+                marked.Add(wantedId);
+            }
+            while (true);
+        }
+    }
+}
