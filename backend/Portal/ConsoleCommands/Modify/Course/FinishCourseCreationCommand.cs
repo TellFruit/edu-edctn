14f3dd02f957f313dcdc776f919e1d4542c9cc64
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-08 09:25:13 +0300

    Finish FinishCourseCreationCommand impl of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Modify/Course/FinishCourseCreationCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Modify.Course
+{
+    internal class FinishCourseCreationCommand : IConsoleCommand
+    {
+        private readonly CourseDTO _courseDTO;
+
+        public FinishCourseCreationCommand(CourseDTO courseDTO)
+        {
+            _courseDTO = courseDTO;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            if (_courseDTO.Materials.Count < 0)
+            {
+                Console.WriteLine("Add materials first!");
+                return true;
+            }
+
+            string input = "";
+
+            Console.WriteLine("Write name of the course: ");
+            input = Console.ReadLine();
+            _courseDTO.Name = input;
+
+            Console.WriteLine("Write description of the course: ");
+            input = Console.ReadLine();
+            _courseDTO.Description = input;
+
+            return false;
+        }
+    }
+}
