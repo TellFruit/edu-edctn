1f8b9de4adc5a740706513d2ebbb35a519ffb31d
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-13 15:11:41 +0300

    Finish LogoutCommand implementation of IConsoleCommand

---
+++ backend/Portal/ConsoleCommands/Auth/LogoutCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.UI_Console.ConsoleCommands.Auth
+{
+    internal class LogoutCommand : IConsoleCommand
+    {
+        private readonly IUserAuth _userAuth;
+
+        public LogoutCommand(IUserAuth userAuth)
+        {
+            _userAuth = userAuth;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            if (await _userAuth.Logout())
+            {
+                Console.Write("Success! You've just logged out.");
+            }
+            else
+            {
+                Console.Write("Failure! You are alredy not logged...");
+            }
+
+            return true;
+        }
+    }
+}
