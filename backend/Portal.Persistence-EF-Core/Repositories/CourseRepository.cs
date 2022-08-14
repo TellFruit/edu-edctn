05a11754b7aff95867c41b9ca6f1df154c0bad10
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-14 23:23:13 +0300

    Start CourseRepository impl of IGenericRepository

---
+++ backend/Portal.Persistence-EF-Core/Repositories/CourseRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Portal.Application.Interfaces.OuterImpl;
+using Portal.Domain.Entities;
+using Portal.Persistence_EF_Core.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence_EF_Core.Repositories
+{
+    internal class CourseRepository : BaseRepository, IGenericRepository<CourseDomain>
+    {
+        public CourseRepository(IMapper mapper, PortalContext context)
+            : base(mapper, context) {}
+
+        public Task<CourseDomain> Create(CourseDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(CourseDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<CourseDomain>> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CourseDomain> Update(CourseDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
