a08280117882613f3edb4fd776c6bf138561c588
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-14 22:30:32 +0300

    Start VideoRepository impl of IGenericRepository

---
+++ backend/Portal.Persistence-EF-Core/Repositories/VideoRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Portal.Application.Interfaces.OuterImpl;
+using Portal.Domain.Entities;
+using Portal.Persistence_EF_Core.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence_EF_Core.Repositories
+{
+    internal class VideoRepository : BaseRepository, IGenericRepository<VideoDomain>
+    {
+        public VideoRepository(IMapper mapper, PortalContext context) 
+            : base(mapper, context) {}
+
+        public Task<VideoDomain> Create(VideoDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(VideoDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<VideoDomain>> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VideoDomain> Update(VideoDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
