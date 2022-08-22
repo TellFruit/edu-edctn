9d1fac43ea2a85d3c2c1941d5f76e7fab15d1872
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-22 21:48:35 +0300

    start perk repository implementation of igenericrepository

---
+++ backend/Portal.Persistence-EF-Core/Repositories/PerkRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence_EF_Core.Repositories
+{
+    internal class PerkRepository : BaseRepository, IGenericRepository<PerkDomain>
+    {
+        public PerkRepository(IMapper mapper, PortalContext context) 
+            : base(mapper, context) {}
+
+        public Task<PerkDomain> Create(PerkDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(PerkDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<PerkDomain>> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PerkDomain> Update(PerkDomain entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
