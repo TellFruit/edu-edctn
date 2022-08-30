a3f755ce8e4f701b342f895f6a2f7516bcadd0a4
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-08-30 16:44:26 +0300

    UserPerk finish Reestimate method and private GetCompletedProgressesByPerk method

--- backend/Portal.Domain/Entities/UserPerk.cs
+++ backend/Portal.Domain/Entities/UserPerk.cs
@@ -11,7 +11,26 @@
 
         public void Reestimate()
         {
+            int res = default;
 
+            var found = GetCompletedProgressesByPerk(PerkId);
+
+            foreach(var completed in found)
+            {
+                res++;
+            }
+
+            if (res > 0)
+            {
+                Level = res;
+            }
+        }
+
+        private ICollection<CourseProgress> GetCompletedProgressesByPerk(int perkId)
+        {
+            return User.CourseProgress
+                .Where(c => c.CourseFinished
+                    && c.Course.Perks.Any(p => p.Id.Equals(perkId))).ToList();
         }
     }
 }
