a0f0afd0761f0652478e2ab09bfa99ab255c5bf4
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-02 02:47:53 +0300

    Finish GetFinishedCoursesCommand impl of IConsoleCommand

---
+++ backend/Portal.UI-Console/ConsoleCommands/Rules/User/GetFinishedCoursesCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace Portal.UI_Console.ConsoleCommands.Rules.User
+{
+    internal class GetFinishedCoursesCommand : IConsoleCommand
+    {
+        private readonly UserDTO _userDTO;
+
+        public GetFinishedCoursesCommand(UserDTO userDTO)
+        {
+            _userDTO = userDTO;
+        }
+
+        public async Task<bool> Run(params string[] parameters)
+        {
+            var finished = _userDTO.CourseProgress
+                .Where(c => c.Progress == Domain.DomainConstants.ProgressFullfiled)
+                .Select(c => c.Course);
+
+            Console.WriteLine("You have finished these courses: ");
+            foreach (var course in finished)
+            {
+                Console.WriteLine(course);
+            }
+
+            return true;
+        }
+    }
+}
