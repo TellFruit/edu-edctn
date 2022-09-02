666eda96d5d0a5b06ade9ecb7a790ff1cbb3077f
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-02 11:40:40 +0300

    finish get learned material command impl of iconsolecommand + user dto add ctor

--- backend/Portal.Application/DTO/UserDTO.cs
+++ backend/Portal.Application/DTO/UserDTO.cs
@@ -12,5 +12,12 @@
         public ICollection<CourseProgressDTO> CourseProgress { get; set; }
         public ICollection<MaterialLearnedDTO> MaterialLearned { get; set; }
         public ICollection<PerkLevelDTO> PerkLevel { get; set; }
+
+        public UserDTO()
+        {
+            CourseProgress = new List<CourseProgressDTO>();
+            MaterialLearned = new List<MaterialLearnedDTO>();
+            PerkLevel = new List<PerkLevelDTO>();
+        }
     }
 }
