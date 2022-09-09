6eac454b26bd040220fa22ee5ebf4f7ca366fc55
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-09 23:03:42 +0300

    finish and specification concrete class of specification<t>

--- backend/Portal.Domain/GenericSpecification/AndSpecification.cs
+++ backend/Portal.Domain/GenericSpecification/AndSpecification.cs
@@ -3,7 +3,21 @@ using Portal.Domain.Interfaces;
 
 namespace Portal.Domain.GenericSpecification
 {
-    internal class AndSpecification<T> : ISpecification<T> where T : BaseEntity
+    internal class AndSpecification<T> : Specification<T> where T : BaseEntity
     {
+        ISpecification<T> leftSpec;
+        ISpecification<T> rightSpec;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            leftSpec = first;
+            rightSpec = second;
+        }
+
+        public override bool IsSatisfiedBy(T item)
+        {
+            return leftSpec.IsSatisfiedBy(item) 
+                && rightSpec.IsSatisfiedBy(item);
+        }
     }
 }
