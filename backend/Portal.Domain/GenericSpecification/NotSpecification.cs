a1ebd74d37bb48401e94c35d41b1cd04c3fd49e7
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-09 23:21:37 +0300

    finish not specification concrete class of specification<t>

--- backend/Portal.Domain/GenericSpecification/NotSpecification.cs
+++ backend/Portal.Domain/GenericSpecification/NotSpecification.cs
@@ -3,7 +3,18 @@ using Portal.Domain.Interfaces;
 
 namespace Portal.Domain.GenericSpecification
 {
-    internal class NotSpecification<T> : ISpecification<T> where T : BaseEntity
+    internal class NotSpecification<T> : Specification<T> where T : BaseEntity
     {
-    }
+		private ISpecification<T> specification;
+
+		public NotSpecification(ISpecification<T> spec)
+		{
+			specification = spec;
+		}
+
+		public override bool IsSatisfiedBy(T item)
+		{
+			return specification.IsSatisfiedBy(item) == false;
+		}
+	}
 }
