538840f4571ce182d7bca8e54989ce9eccd71195
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-09 22:24:03 +0300

    create specification extension class with where and first or default methods

---
+++ backend/Portal.Domain/GenericSpecification/SpecificationExtension.cs
@@ -0,0 +1,22 @@
+﻿using Portal.Domain.Entities.Abstract;
+using Portal.Domain.Interfaces;
+
+namespace Portal.Domain.GenericSpecification
+{
+    public static class SpecificationExtension
+    {
+        public static ICollection<T> Where<T>(this ICollection<T> collection,
+            ISpecification<T> specification) where T : BaseEntity
+        {
+            return collection
+                .Where(e => specification.IsSatisfiedBy(e))
+                .ToList();
+        }
+
+        public static T? FirstOrDefault<T>(this ICollection<T> collection,
+            ISpecification<T> specification) where T : BaseEntity
+        {
+            return collection.FirstOrDefault(e => specification.IsSatisfiedBy(e));
+        }
+    }
+}
