3193ca704de19297f090a8d151718a124210efca
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-14 20:53:33 +0300

    make partial.js and ajax jquery for article table work

--- backend/Portal.UI-MVC-Web/wwwroot/js/site.js
+++ backend/Portal.UI-MVC-Web/wwwroot/js/site.js
@@ -1,13 +1,1 @@
-﻿function getPartial(url, data, destination) {
-    $ajax({
-        type: "Get",
-        url: url,
-        data: data,
-        traditional: true,
-        success: function (result) {
-            $(destination).html(result);
-        }
-    });
-}
-
-export { getPartial };
+﻿
