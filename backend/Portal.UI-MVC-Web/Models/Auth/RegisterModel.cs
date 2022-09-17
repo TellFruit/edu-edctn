2a1dc6e3aae1aa20e25190914caf422605d46af5
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-17 17:04:20 +0300

    Create RegisterModel class

---
+++ backend/Portal.UI-MVC-Web/Models/Auth/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Portal.UI_MVC_Web.Models.Auth
+{
+    public class RegisterModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
