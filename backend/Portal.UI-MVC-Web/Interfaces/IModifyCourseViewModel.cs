417a874437fb16ae0edd85729d0825b1fe2fde29
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-19 18:31:16 +0300

    Start IModifyCourseViewModel interface

---
+++ backend/Portal.UI-MVC-Web/Interfaces/IModifyCourseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Portal.UI_MVC_Web.Models.Course;
+
+namespace Portal.UI_MVC_Web.Interfaces
+{
+    public interface IModifyCourseViewModel
+    {
+        public void SetHttpContext(HttpContext context);
+
+        public void SetModelFromDTO(CourseDTO courseDTO);
+
+        public void AddArticle(int id);
+
+        public void RemoveArticle(int id);
+
+        public ModifyCourseModel GetModel();
+    }
+}
