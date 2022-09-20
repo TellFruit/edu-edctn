ffd089c22c3257e7772cd2a0783d1cf862bc37c1
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-20 12:59:26 +0300

    Finish BookController actions

--- backend/Portal.UI-MVC-Web/Controllers/BookController.cs
+++ backend/Portal.UI-MVC-Web/Controllers/BookController.cs
@@ -2,5 +2,74 @@
 {
     public class BookController : Controller
     {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await GetArticleIndexModel());
+        }
+
+        public async Task<IActionResult> AddOrEdit(int? id)
+        {
+            BookDTO book;
+
+            if (id == null)
+            {
+                book = new BookDTO();
+            }
+            else
+            {
+                book = await _bookService.GetById(id.Value);
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(BookDTO book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(book);
+            }
+
+            if (book.Id.Equals(default))
+            {
+                await _bookService.Create(book);
+            }
+            else
+            {
+                await _bookService.Update(book);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _bookService.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return PartialView("_ArticlePartial", await GetArticleIndexModel());
+        }
+
+        private async Task<BookIndexModel> GetArticleIndexModel()
+        {
+            var articles = await _bookService.GetAll();
+
+            return new BookIndexModel(articles);
+        }
     }
 }
