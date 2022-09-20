6af3950fd3ee1c847cbc42de7e4566a39015ec99
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-20 17:36:22 +0300

    Create VideoIndexModel

---
+++ backend/Portal.UI-MVC-Web/Models/Materials/VideoIndexModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Portal.UI_MVC_Web.Models.Materials
+{
+    public class VideoIndexModel
+    {
+        public ICollection<VideoDTO> Videos { get; set; }
+
+        public VideoIndexModel(ICollection<VideoDTO> videos)
+        {
+            Videos = videos;
+        }
+    }
+}
