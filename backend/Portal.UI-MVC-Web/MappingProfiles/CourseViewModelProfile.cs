572b5c151a15002a634fcbb2d049675e7fae3e26
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-21 14:36:18 +0300

    Create mapping profiles for CourseViewModel and CourseArticleModel

---
+++ backend/Portal.UI-MVC-Web/MappingProfiles/CourseViewModelProfile.cs
@@ -0,0 +1,24 @@
+﻿namespace Portal.UI_MVC_Web.MappingProfiles
+{
+    public class CourseViewModelProfile : Profile
+    {
+        public CourseViewModelProfile()
+        {
+            CreateMap<CourseViewModel, CourseDTO>();
+
+            CreateMap<CourseDTO, CourseViewModel>()
+                .ForMember(vm => vm.Articles, 
+                    src => src.MapFrom(
+                        dto => dto.Materials.OfType<ArticleDTO>()
+                            .Select(a => new CourseArticleModel()
+                            {
+                                Id = a.Id,
+                                Title = a.Title,
+                                Published = a.Published,
+                                Url = a.Url,
+                                IsSelected = true
+                            })
+                            .ToList()));
+        }
+    }
+}
--- backend/Portal.UI-MVC-Web/ProjectUsing.cs
+++ backend/Portal.UI-MVC-Web/ProjectUsing.cs
@@ -1,10 +1,12 @@
 ﻿global using System.Security.Claims;
+global using AutoMapper;
 
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Authentication.Cookies;
 global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Authentication;
 
+global using Portal.UI_MVC_Web.Models.Course;
 global using Portal.UI_MVC_Web.Models.Materials;
 global using Portal.UI_MVC_Web.Models.Auth;
 global using Portal.Application.DTO;
