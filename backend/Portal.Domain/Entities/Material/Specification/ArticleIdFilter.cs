818967ba88fcf40c151b7428e861a04b526cf356
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-21 17:26:56 +0300

    Create ArticleIdFilter specification

---
+++ backend/Portal.Domain/Entities/Material/Specification/ArticleIdFilter.cs
@@ -0,0 +1,19 @@
+﻿using Portal.Domain.GenericSpecification;
+
+namespace Portal.Domain.Entities.Material.Specification
+{
+    public class ArticleIdFilter : Specification<ArticleDomain>
+    {
+        private ICollection<int> _ids;
+
+        public ArticleIdFilter(ICollection<int> ids)
+        {
+            _ids = ids;
+        }
+
+        public override bool IsSatisfiedBy(ArticleDomain item)
+        {
+            return _ids.Contains(item.Id);
+        }
+    }
+}
