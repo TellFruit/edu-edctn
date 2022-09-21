def122caf51a817d04ca57ccb2a378f415ab2b3f
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-21 14:07:24 +0300

    Create CourseViewModel and CourseArticleModel

---
+++ backend/Portal.UI-MVC-Web/Models/Course/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Portal.UI_MVC_Web.Models.Course
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<CourseArticleModel> Articles { get; set; }
+
+        public CourseViewModel()
+        {
+            Articles = new List<CourseArticleModel>();
+        }
+    }
+}
