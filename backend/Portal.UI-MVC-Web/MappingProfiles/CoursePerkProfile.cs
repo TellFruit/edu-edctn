d081ac953c8ec8bae2ed9e69d24e45dc96b69086
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-22 18:24:53 +0300

    Create CoursePerkProfile profile

---
+++ backend/Portal.UI-MVC-Web/MappingProfiles/CoursePerkProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace Portal.UI_MVC_Web.MappingProfiles
+{
+    public class CoursePerkProfile : Profile
+    {
+        public CoursePerkProfile()
+        {
+            CreateMap<CoursePerkModel, PerkDTO>();
+
+            CreateMap<PerkDTO, CoursePerkModel>();
+        }
+    }
+}
