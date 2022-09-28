81b189438ec2668c1ff0ee497a05ab4cd0339916
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-09-28 15:44:14 +0300

    Create CourseValidator

---
+++ backend/Portal.Application/Validation/CourseValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace Portal.Application.Validation
+{
+    public class CourseValidator : AbstractValidator<CourseDTO>
+    {
+        public CourseValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty();
+
+            RuleFor(c => c.Description)
+                .NotEmpty();
+
+            RuleFor(c => c.Materials)
+                .Must(m => m.Count > 0);
+
+            RuleFor(c => c.Perks)
+                .Must(p => p.Count > 0);
+        }
+    }
+}
